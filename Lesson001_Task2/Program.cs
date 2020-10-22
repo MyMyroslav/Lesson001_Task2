b4f4ee92d627787859a239c81bfeeaaac02928c0
Myroslav <havrushkomyroslav@gmail.com>
2020-10-22 20:57:48 +0300

    Done-Lesson001_Task2

---
+++ Lesson001_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson001_Task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int iYear = 2002;
+            Console.WriteLine(iYear);
+        }
+    }
+}
